ae1193b8422f6692c3d02b46539f7f648c80949f
conradj <conradsjackson@gmail.com>
2018-09-26 22:42:51 +0100

    improve train cost and driving time calcs

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,67 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var axios = require('axios');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var axios = require("axios");
 
 var app = express();
 
 const PENCE_PER_KM = {
   driving: 25,
   walking: 0,
-  transit: 150,
+  transit: 17,
   bicycling: 1
-}
+};
 
 const CALORIES_PER_KM = {
   driving: 0,
   walking: 75,
   transit: 0,
   bicycling: 150
-}
+};
 
 const CO2_PER_KM = {
   driving: 0.257,
   walking: 0,
   transit: 0.0602,
   bicycling: 0
-}
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/directions', async (request, response) => {
+app.get("/directions", async (request, response) => {
+  const { origin, destination, mode } = request.query;
+  const morningCommuteDate = new Date();
+  morningCommuteDate.setDate(morningCommuteDate.getDate() + 1);
+  morningCommuteDate.setHours(7);
+  morningCommuteDate.setMinutes(28);
+  morningCommuteDate.setMilliseconds(0);
+  const departureTime = morningCommuteDate.getTime() / 1000;
 
-  const { origin, destination, mode } = request.query
-  const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=${mode}&key=AIzaSyAWGw_OqX8KLR5HWQS7aryq9CVRdDw_BR4&alternatives=true`;
-  const { data } = await axios.get(url)
+  const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&mode=${mode}&departure_time=${departureTime}&traffic_model=pessimistic&key=AIzaSyAWGw_OqX8KLR5HWQS7aryq9CVRdDw_BR4&alternatives=true`;
+  const { data } = await axios.get(url);
 
   const parseRouteResult = routeResult => {
-
-    const route = routeResult.routes[0]
-    const leg = route.legs[0]
+    const route = routeResult.routes[0];
+    const leg = route.legs[0];
 
     return {
       start_address: leg.start_address,
       end_address: leg.end_address,
       distance: leg.distance,
       mode,
-      duration: leg.duration,
+      duration: leg.duration_in_traffic
+        ? leg.duration_in_traffic
+        : leg.duration,
       cost: Math.round((leg.distance.value / 1000) * PENCE_PER_KM[mode]),
       calories: Math.round((leg.distance.value / 1000) * CALORIES_PER_KM[mode]),
       co2: (leg.distance.value / 1000) * CO2_PER_KM[mode]
-    }
-  }
+    };
+  };
 
-  response.json(parseRouteResult(data))
+  response.json(parseRouteResult(data));
+});
 
-})
-
-app.listen(3001)
-
+app.listen(3001);
