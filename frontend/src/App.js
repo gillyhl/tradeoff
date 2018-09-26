fc727e9dceae3caf3ddf903077d87521c8c37aa2
Doug McDonald <dougajmcdonald@gmail.com>
2018-09-26 10:18:02 +0100

    Began to add RoutesResultsContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,35 @@ import './App.css';
 import Map from './modules/map/Map'
 import Header from './modules/layout/Header'
 import Sidebar from './modules/layout/Sidebar'
+import RouteReseultsContext from './RouteResultsContext'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      routeResults: []
+    }
+  }
+
+  addRouteResult(routeResult){
+    this.setState({
+      routeResults:[
+        ...this.state.routeResults,
+        routeResult
+      ]
+    })
+  }
+
+
   render() {
     return (
-      <div>
+      <RoutesResultsContext.Provider value={{
+        results:this.state.routeResults
+      }} >
         <Header />
         <Sidebar />
         <Map />
-      </div>
+      </RoutesResultsContext.Provider>
     );
   }
 }
