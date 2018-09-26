8888e6640925cb37fa6d215e887786c32aac7e64
Doug McDonald <dougajmcdonald@gmail.com>
2018-09-26 13:17:34 +0100

    show cost in punds #CJ

--- frontend/src/modules/map/RouteResult.js
+++ frontend/src/modules/map/RouteResult.js
@@ -7,7 +7,7 @@ const RouteResult = ({ duration, mode, distance, cost, calories }) => {
       <li><strong>Mode:</strong> {mode}</li>
       <li><strong>Duration:</strong> {duration}</li>
       <li><strong>Distance:</strong> {distance}</li>
-      <li><strong>Cost:</strong> {cost}</li>
+      <li><strong>Cost:</strong> &pound;{cost / 100}</li>
       <li><strong>Calories:</strong> {calories}</li>
     </ul>
   </section>
